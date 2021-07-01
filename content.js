f5fac3888bab465a60d497d83c29c58112ba0d13
Aadhi Nana <ajibaa@gmail.com>
2021-07-01 20:13:37 +0530

    feat: set custom speed for videos

--- content.js
+++ content.js
@@ -1,13 +1,32 @@
-chrome.runtime.sendMessage(
-  { message: "Hello from the content script! 🎉" },
-  function (response) {
-    console.log("This is received from the bg script -> ");
-    console.log(response.message);
-  }
-);
+// SEND MESSAGES TO BG
+// chrome.runtime.sendMessage(
+//   { message: "Hello from the content script! 🎉" },
+//   function (response) {
+//     console.log("This is received from the bg script -> ");
+//     console.log(response.message);
+//   }
+// );
+console.log("Content script");
 
+const GLOBAL_STATE = {
+  speed: 1,
+};
+
+// RECEIVE MESSAGES FROM BG
 chrome.runtime.onMessage.addListener((req, sender, sendMessage) => {
-  console.log(req.message);
+  console.log(req);
+  GLOBAL_STATE.speed = req.speed;
   // console.log(sender); //sender => id -> id of the extension
   sendMessage("Got it!");
+  return true;
 });
+
+function keepChecking() {
+  const video = document.querySelector("video");
+  if (!video) return;
+  console.log(video.playbackRate + " current playback rate this is");
+  if (video.playbackRate == GLOBAL_STATE.speed) return;
+  video.playbackRate = GLOBAL_STATE.speed;
+}
+
+setInterval(keepChecking, 2000);
--- popup.js
+++ popup.js
@@ -5,44 +5,36 @@ console.log("This loads only once?");
 const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
-let speed;
-
 function handleSubmit(e) {
   e.preventDefault();
-  speed = e.target.querySelector("#speed").value;
+  let speed = e.target.querySelector("#speed").value;
   console.log(`${speed} is set as speed`);
-  // console.log(speed);
-  // console.log(`Submitted form with value ${speed}`);
-  // chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-  //   const tab = tabs[0];
-  //   console.log(tab);
-  //   console.log(tab.title);
-  //   chrome.tabs.sendMessage(tab.id, {
-  //     message: `Hello I'm trying to send you stuff!`,
-  //   });
-  // });
+
+  // Send the speed set over to the content script
+  chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    const tab = tabs[0];
+    chrome.tabs.sendMessage(tab.id, {
+      message: `Hello I'm trying to send you stuff!`,
+      speed,
+    });
+  });
 }
 
-// Make sure content.js is loaded before this
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request.message);
   // console.log(sender); // sender => id, origin, tab: Tab, url
   sendResponse({ message: "Hello from the BG script 🎉" });
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, { status }, tab) {
-  if (status === "complete") {
-    chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-      const tab = tabs[0];
-      chrome.tabs.sendMessage(
-        tab.id,
-        {
-          message: `Hello I'm trying to send you stuff!`,
-        },
-        (res) => {
-          console.log(res);
-        }
-      );
-    });
-  }
-});
+// WAIT FOR THE TAB TO LOAD ITS CONTENT SCRIPT THEN SEND IT MESSAGES.
+// chrome.tabs.onUpdated.addListener(function (tabId, { status }, tab) {
+//   console.log("Sending");
+//   if (status === "complete") {
+//     chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+//       const tab = tabs[0];
+//       chrome.tabs.sendMessage(tab.id, {
+//         speed,
+//       });
+//     });
+//   }
+// });
